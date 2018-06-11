d7c6bac1e73aafb4d963587cc57080c9512f3925
AlexanderPokidin <a.pokidin@gmail.com>
2018-06-11 18:31:38 +0300

    Application is done. Dependence fixed

--- app/src/main/java/com/pokidin/a/sunrisesunsetapp/MainActivity.java
+++ app/src/main/java/com/pokidin/a/sunrisesunsetapp/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SunriseSunsetActivity.class);
                 startActivity(intent);
-
             }
         });
     }
